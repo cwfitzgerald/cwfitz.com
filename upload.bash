c1b35ac21f5ea4c3d59deb3ced6b12a8a50bea34
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-15 01:58:18 -0400

    Use the bash script for uploading

--- upload.bash
+++ upload.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -r -z --delete --chmod u=rwX,go=rX --progress $(cat files) connor@cwfitz.com:/var/www/cwfitz
+rsync -r -z --delete --chmod u=rwX,go=rX -e "ssh -oStrictHostKeyChecking=accept-new" --progress $(cat files) connor@cwfitz.com:/var/www/cwfitz
