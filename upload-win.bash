64e55e01319c502bd8c4992c6d77a7ac1dd0c392
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-15 01:59:47 -0400

    Get scripts to terminate on failure

--- upload-win.bash
+++ upload-win.bash
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+
+set -e
+
 scp -r $(cat files | tr -d '\r') cwfitz.com:/var/www/cwfitz
--- upload.bash
+++ upload.bash
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+
+set -e
+
 rsync -r -z --delete --chmod u=rwX,go=rX -e "ssh -oStrictHostKeyChecking=accept-new" --progress $(cat files) connor@cwfitz.com:/var/www/cwfitz
